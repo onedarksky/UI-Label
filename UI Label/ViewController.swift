361021cbb17f582e408033bc4bf094e611ffab0c
Ron <onedarksky@icloud.com>
2021-09-04 09:27:09 +0800

    Initial Commit

---
+++ UI Label/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI Label
+//
+//  Created by 江庸冊 on 2021/9/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
